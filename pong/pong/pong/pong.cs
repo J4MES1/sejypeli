df8ebe08d75c29c4094c613ccd6bc6a642cb83e8
J4MES1 <braptorx@gmail.com>
2013-09-12 08:13:07 -0700

    Hieno pong

---
+++ pong/pong/pong/pong.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class pong : PhysicsGame
+{
+    PhysicsObject pallo;
+    int x = 1000;
+    public override void Begin()
+    {
+        Luokentta();
+        AloitaPeli();
+        
+        
+        // TODO: Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    void Luokentta()
+    {
+        pallo = new PhysicsObject(50, 50);
+        pallo.Shape = Shape.Circle;
+        pallo.X = 200.0;
+        pallo.Y = 0.0;
+        pallo.Restitution = 1.0;
+        pallo.Color = Color.Red;
+        Add(pallo);
+        
+        LuoMaila(Level.Left + 20.0, 0.0);
+        LuoMaila(Level.Right - 20.0, 0.0);
+
+        Level.CreateBorders(1.0, false);
+        Level.BackgroundColor = Color.Blue;
+
+        Camera.ZoomToLevel();
+    }
+    void AloitaPeli()
+    {
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi);
+    }
+    void LuoMaila(double x, double y)
+    {
+        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0);
+        maila.Shape = Shape.Rectangle;
+        maila.X = x;
+        maila.Y = y;
+        maila.Restitution = 1.0;
+        Add(maila);
+    }
+    void AsetaOhjaimet()
+    {
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}
+
